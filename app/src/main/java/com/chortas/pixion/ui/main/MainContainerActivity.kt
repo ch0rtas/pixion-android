675abd0f4f5363815ccd2878e5465667df98e3a6
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-05-03 04:09:58 +0200

    Doble toque para salir de la app

--- app/src/main/java/com/chortas/pixion/ui/main/MainContainerActivity.kt
+++ app/src/main/java/com/chortas/pixion/ui/main/MainContainerActivity.kt
@@ -1,6 +1,9 @@
 package com.chortas.pixion.ui.main
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.chortas.pixion.R
@@ -8,6 +11,8 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainContainerActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private var doubleBackToExitPressedOnce = false
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +37,17 @@ class MainContainerActivity : AppCompatActivity() {
 
         if (!navController.popBackStack()) {
             if (auth.currentUser != null) {
-                // Si estamos en el fragmento principal y el usuario está autenticado, no hacer nada
+                if (doubleBackToExitPressedOnce) {
+                    finish()
+                    return
+                }
+
+                doubleBackToExitPressedOnce = true
+                Toast.makeText(this, getString(R.string.press_back_again_to_exit), Toast.LENGTH_SHORT).show()
+
+                handler.postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000) // 2 segundos para presionar de nuevo
                 return
             }
             super.onBackPressed()
